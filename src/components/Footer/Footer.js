055ca8a3786b587cffe86ba2b97b16d86743cb2d
Michael Carr <mcarr2468@gmail.com>
2023-01-23 12:20:51 -0600

    changed the slogan

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,7 +25,7 @@ const Footer = () => {
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>Innovating one project at a time</Slogan>
+          <Slogan>Innovative solutions for a digital world</Slogan>
         </CompanyContainer>
         <SocialContainer>
           <SocialIcons href="https://github.com/Michael-c7">
