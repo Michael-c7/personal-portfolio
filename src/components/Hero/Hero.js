4a8ac472b04e0979cd904f5ed168a088ae499dce
Michael Carr <mcarr2468@gmail.com>
2023-01-23 14:05:06 -0600

    changed the text on the button from Learn More to Downlaod Resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,8 +13,9 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        I am a web developer with a passion for creating visually appealing and user-friendly websites. My goal is to design websites that not only look great, but also provide a seamless experience for the user. I am always looking for new challenges and opportunities to expand my knowledge and skills.        </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        I am a web developer with a passion for creating visually appealing and user-friendly websites. My goal is to design websites that not only look great, but also provide a seamless experience for the user. I am always looking for new challenges and opportunities to expand my knowledge and skills.
+        </SectionText>
+        <Button onClick={props.handleClick}>Download Resume</Button>
       </LeftSection>
     </Section>
   </>
