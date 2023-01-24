36fbce5a6725ba73fd6a612bcf970cb45645cde5
Michael Carr <mcarr2468@gmail.com>
2023-01-24 08:33:11 -0600

    created the functionality for the download resume button to download a resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,24 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
+const handleDownload = () => {
+  const filePath = '/test-template.pdf';
+  /*MAKE SURE TO PUT YOUR REAL RESUME IN HERE!!! */
+  fetch(filePath)
+    .then(response => response.blob())
+    .then(blob => {
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      // the name of the file when you download it, the name can be anything eg: eggs.pdf, cat.pdf
+      link.download = "test-template.pdf";
+      link.click();
+    });
+};
+
 const Hero = (props) => (
+
+
+  
   <>
     <Section row nopadding>
       <LeftSection>
@@ -15,7 +32,11 @@ const Hero = (props) => (
         <SectionText>
         I am a web developer with a passion for creating visually appealing and user-friendly websites. My goal is to design websites that not only look great, but also provide a seamless experience for the user. I am always looking for new challenges and opportunities to expand my knowledge and skills.
         </SectionText>
-        <Button onClick={props.handleClick}>Download Resume</Button>
+        {/* <a href="" download>
+          <Button>Download Resume</Button>
+        </a> */}
+        <Button onClick={handleDownload}>Download Resume</Button>
+
       </LeftSection>
     </Section>
   </>
